f2737f61c90a4670b3533ea01d3e265462704a0a
Yuri <yuridacostagouveia@gmail.com>
2020-03-22 14:52:50 -0300

    Development: Transiction effect into card.

--- src/app/components/home/home.component.js
+++ src/app/components/home/home.component.js
@@ -5,6 +5,11 @@ angular.
   component('home', {  
       templateUrl: `${__dirname}/src/app/components/home/home.view.html`,
         controller: function HomeController() {
-            this.path = path.join(process.cwd(), '/src/assets/img/logo.png')
+            this.path = path.join(process.cwd(), '/src/assets/img/logo.png');
+
+            this.eventClick = function() {
+              const element = document.getElementById('card-processos');
+              element.classList.add('scale-down-center');
+            }
     },
 });
