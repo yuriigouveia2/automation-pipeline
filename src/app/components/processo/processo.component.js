98ad8e650b9f96ccb2657bc3d4a2355e494ec19e
Yuri <yuridacostagouveia@gmail.com>
2020-04-13 20:52:57 -0300

    Implementation: process search by port number

--- src/app/components/processo/processo.component.js
+++ src/app/components/processo/processo.component.js
@@ -1,11 +1,23 @@
+const shell = require('shelljs');
+shell.config.execPath = shell.which('node').toString();
+
 angular.
   module('automation').
   component('processo', { 
     templateUrl: `${__dirname}/src/app/components/processo/processo.view.html`,
     controller: function ProcessoController() {
+      this.porta = null;
       this.computadorIcon = path.join(process.cwd(), '/src/assets/icons/computador-100.png');
+      this.lupaIcon = path.join(process.cwd(), '/src/assets/icons/lupa-48.png');
+      
       this.procuraProcesso = function() {
-          
+        if (this.porta) {
+          shell.exec(`netstat -ano | findStr :${this.porta}`, (err, data) => {
+            console.log(data);
+          });
+        } else {
+          shell.exec(`netstat -ano`);
+        }
       }
     }
-   });
+  });
