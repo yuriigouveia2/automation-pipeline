d67fc05071baacb56073d840b056eabc91745fe0
yuriigouveia2 <yuri.gouveia@eng.ci.ufpb.br>
2020-04-13 19:21:46 -0300

    Implementation: screen form

--- src/app/components/processo/processo.component.js
+++ src/app/components/processo/processo.component.js
@@ -3,8 +3,9 @@ angular.
   component('processo', { 
     templateUrl: `${__dirname}/src/app/components/processo/processo.view.html`,
     controller: function ProcessoController() {
-        this.procuraProcesso = function() {
-            
-        }
+      this.computadorIcon = path.join(process.cwd(), '/src/assets/icons/computador-100.png');
+      this.procuraProcesso = function() {
+          
+      }
     }
    });
